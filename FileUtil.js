c0e3107ec87ed993be1ac29368738c8226e64309
ShaneStevenLei <shanestevenlei@outlook.com>
2015-09-23 15:07:47 +0800

    Finish the file util.
    Signed-off-by: ShaneStevenLei <shanestevenlei@outlook.com>

---
+++ FileUtil.js
@@ -0,0 +1,65 @@
+/**
+ * Html5 file util, need JQuery and SparkMD5(https://github.com/satazor/SparkMD5)
+ * 
+ * Usage:
+ *      1. var fileUtil = new FileUtil();
+ *      2. Check the browser whether support: 
+ *          if(!fileUtil.support){
+ *              console.log(FileUtil isn't supported!);
+ *              alert('FileUtil isn't supported!'); 
+ *          }
+ *      3. Get file md5:
+ *          // Get the file md5 successfully
+ *          fileUtil.get(file).done(function(md5){
+ *              console.log('Get the file md5 successfully: ' + md5);
+ *          });
+ *          // Getting the file md5 in the progress
+ *          fileUtil.getMd5(file.file).progress(function(){
+ *              console.log('Getting the file md5.');
+ *          });
+ *          // Get the file md5 unsuccessfully
+ *          fileUtil.getMd5(file.file).fail(function(){
+ *              console.log('Get the file md5 unsuccessfully.');
+ *          });
+ *
+ * @author ShaneStevenLei[shanestevenlei@outlook.com]
+ * 
+ * @license based on MIT LICENSE
+ * 
+ */
+var FileUtil = function() {}
+FileUtil.prototype = {
+    support: (typeof(File) !== 'undefined') &&
+        (typeof(Blob) !== 'undefined') &&
+        (typeof(FileList) !== 'undefined') &&
+        (!!Blob.prototype.webkitSlice || !!Blob.prototype.mozSlice || !!Blob.prototype.slice || false),
+    getMd5: function(file) {
+        var blobSlice = File.prototype.slice || File.prototype.mozSlice || File.prototype.webkitSlice,
+            chunkSize = 2 * 1024 * 1024, // Read in chunks of 2MB
+            chunks = Math.ceil(file.size / chunkSize),
+            currentChunk = 0,
+            spark = new SparkMD5.ArrayBuffer(),
+            fileReader = new FileReader(),
+            defer = $.Deferred();
+        fileReader.onload = function(e) {
+            defer.notify('read chunk # ' + (currentChunk + 1) + ' of ' + chunks);
+            spark.append(e.target.result); // Append array buffer
+            currentChunk++;
+            if (currentChunk < chunks) {
+                loadNext();
+            } else {
+                defer.resolve(spark.end());
+            }
+        };
+        fileReader.onerror = function() {
+            defer.reject('oops, something went wrong.');
+        };
+        var loadNext = function() {
+            var start = currentChunk * chunkSize,
+                end = ((start + chunkSize) >= file.size) ? file.size : start + chunkSize;
+            fileReader.readAsArrayBuffer(blobSlice.call(file, start, end));
+        };
+        loadNext();
+        return defer.promise();
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # FileUtil
-HTML5 File Utils
+
+FileUtil is HTML5 file utils.
+
+## Quick start
+Several quick start options are available:
+*   Download [JQuery](http://jquery.com/download)
+*   Download [SparkMD5](https://github.com/satazor/SparkMD5)
+*   Import JQuery and SparkMD5 to your project
+*   Import FileUtil.js
+
+## Usage
+* Import the javascript
+```
+<script src="/project/jquery/jquery.min.js"></script>
+<script src="/project/spark-md5/spark-md5.min.js"></script>
+```
+*  New FileUtil
+```
+var fileUtil = new FileUtil();
+```
+*  Check the browser whether support
+```
+if (!fileUtil.support) {
+    console.log('FileUtil isn't supported!');
+    alert('FileUtil isn't supported!');
+    return;
+}
+```
+*  Get the file md5 successfully
+```
+fileUtil.getMd5(file).done(function(data) {
+    console.log('Get the file md5 successfully: ' + data);
+});
+```
+* Getting the file md5 in the progress
+```
+fileUtil.getMd5(file).progress(function(){
+    console.log('Getting the file md5 in the progress.');
+});
+```
+* Get the file md5 unsuccessfully
+```
+fileUtil.getMd5(file).fail(function(){
+    console.log('Get the file md5 unsuccessfully.');
+});
+```
+
+### License
+FileUtil is open-sourced software licensed under the [MIT license](https://github.com/ShaneStevenLei/FileUtil/blob/master/LICENSE)
